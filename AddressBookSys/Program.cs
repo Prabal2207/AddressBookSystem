1f79b99c952dc4061a97ec9704ee805772c5e379
Prabal2207 <prabal760@gmail.com>
2021-08-04 05:42:48 +0530

    [Prabal] Added:-Delete contact by using its name

--- AddressBookSys/Program.cs
+++ AddressBookSys/Program.cs
@@ -7,9 +7,16 @@ namespace Address_Book_System
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System");
+
             AddressBook.AddContact();
+            Console.WriteLine("\n");
             AddressBook.ContactsDisplay();
+            Console.WriteLine("\n");
             AddressBook.EditContact();
+            Console.WriteLine("\n");
+            AddressBook.ContactsDisplay();
+            Console.WriteLine("\n");
+            AddressBook.DeleteContact();
             AddressBook.ContactsDisplay();
         }
     }
